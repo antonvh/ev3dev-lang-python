c795c97343107f48d86251dc6d81c3aa605e06ce
Anton Vanhoucke <Anton@fabrique.nl>
2016-03-10 23:20:59 +0100

    test

--- speedtest.py
+++ speedtest.py
@@ -3,6 +3,18 @@ __author__ = 'anton'
 # Run 1000 file accesses and measure time
 
 import time
-import ev3dev.ev3 as ev3
+import ev3dev.auto as ev3
 
+m = ev3.Motor(ev3.OUTPUT_A)
+s = ev3.Sensor(ev3.INPUT_1)
 
+t = time.time()
+
+for i in range(1000):
+    p = m.position
+    v = s.value()
+    m.duty_cycle_sp = 30
+
+td = time.time() - t
+
+print "Read/write time per action:", td
